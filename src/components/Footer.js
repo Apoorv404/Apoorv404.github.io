87ba2e4fe65f386fb205631361b35fdde655e591
Apoorv404 <jadhavapoorv2244@gmail.com>
2025-09-28 11:26:36 +0530

    fixed footer size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container } from "react-bootstrap";
 import { FaGithub, FaLinkedin, FaHackerrank } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
